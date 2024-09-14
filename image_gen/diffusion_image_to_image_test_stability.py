e04ce5663d8ade584bdb93b296fda095a789acbd
Henrique da Silva Gameiro <dash@login.cluster>
2024-09-14 12:09:54 +0200

    added code for image gen

--- image_gen/diffusion_image_to_image_test_stability.py
+++ image_gen/diffusion_image_to_image_test_stability.py
@@ -1,18 +1,22 @@
 import torch
-#from diffusers import StableDiffusionImg2ImgPipeline, DPMSolverMultistepScheduler
+from diffusers import StableDiffusionImg2ImgPipeline, DPMSolverMultistepScheduler
 from PIL import Image
-from diffusers import FluxPipeline, DPMSolverMultistepScheduler
 
 def generate_image_from_image(prompt, input_image_path, output_path, strength=0.75):
     # Use the smaller stable-diffusion-2-base model
     #model_id = "stabilityai/stable-diffusion-2-base"
     #model_id = "stabilityai/stable-diffusion-3-medium"
-
     #model_id = "stabilityai/stable-diffusion-xl-base-1.0"
+    model_id = "CompVis/stable-diffusion-v1-4"
 
-    model_id = "black-forest-labs/FLUX.1-dev"
 
-    pipe = FluxPipeline.from_pretrained("black-forest-labs/FLUX.1-dev", torch_dtype=torch.bfloat16)
+    # Load the pipeline with low-memory optimizations
+    pipe = StableDiffusionImg2ImgPipeline.from_pretrained(
+        model_id,
+        torch_dtype=torch.float16,
+        #variant="fp16",
+        #revision="fp16"
+    )
 
     # Use the DPMSolverMultistepScheduler for faster inference
     pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
@@ -23,19 +27,14 @@ def generate_image_from_image(prompt, input_image_path, output_path, strength=0.
     # Move the pipeline to GPU if available
     device = "cuda" if torch.cuda.is_available() else "cpu"
     pipe = pipe.to(device)
-    #pipe = pipe.enable_model_cpu_offload()
+    #pipe.enable_model_cpu_offload()
 
     # Load and preprocess the input image
-    #init_image = Image.open(input_image_path).convert("RGB")
-    #init_image = init_image.resize((768, 768))
+    init_image = Image.open(input_image_path).convert("RGB")
+    init_image = init_image.resize((768, 768))
 
     # Generate the image
-    image = pipe(prompt=prompt,
-        height=1024,
-        width=1024,
-        guidance_scale=3.5,
-        num_inference_steps=50,
-        max_sequence_length=512,).images[0]
+    image = pipe(prompt=prompt, image=init_image, strength=strength, guidance_scale=7.5).images[0]
 
     # Save the image
     image.save(output_path)
@@ -44,7 +43,7 @@ def generate_image_from_image(prompt, input_image_path, output_path, strength=0.
 if __name__ == "__main__":
     #prompt = input("Enter your image prompt: ")
     #input_image_path = input("Enter the path to your input image: ")
-    output_path = "hackathon_images_ai/generated_image_flux.png"
+    output_path = "hackathon_images_ai/generated_image_stability.png"
     prompt = "Mr. Bean with a hat"
     input_image_path = "hackathon_images_base/mr_bean.jpg"
     generate_image_from_image(prompt, input_image_path, output_path)
